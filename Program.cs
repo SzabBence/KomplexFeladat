6ec87c3e5a8e622952a2d35d4c68244e7f6b2369
SzabBence <91546960+SzabBence@users.noreply.github.com>
2024-01-10 21:59:55 +0100

    ezzel már jó

--- Program.cs
+++ Program.cs
@@ -5,10 +5,10 @@ namespace KomplexFeladat
 {
     public class MinTarolo
     {
-        public readonly int minErt;
-        public readonly int minInd;
+        public readonly int[] minErt;
+        public readonly int[] minInd;
 
-        public MinTarolo(int minErt, int minInd)
+        public MinTarolo(int[] minErt, int[] minInd)
         {
             this.minErt = minErt;
             this.minInd = minInd;
@@ -64,21 +64,49 @@ namespace KomplexFeladat
             return folytat;
         }
 
-        static MinTarolo MinKeres(int nap,int N, ref int[,]? ADAT)
+        static int MinIndexTomb(ref int[,]? ADAT,int nap,int N)
         {
-            int minErtek = ADAT[0, nap];
-            int minIndex = 0;
-
-            for(int i = 1; i < N; i++)
+            int minErtek = ADAT[0,nap];
+            for(int i = 0; i<N; i++)
             {
-                if (ADAT[i,nap] < minErtek)
+                if (minErtek > ADAT[i,nap])
                 {
                     minErtek = ADAT[i, nap];
-                    minIndex = i;
                 }
             }
 
-            MinTarolo minTarolo = new MinTarolo(minErtek, minIndex);
+            return minErtek;
+        }
+      
+        static MinTarolo MinKeres(int nap,int N, ref int[,]? ADAT)
+        {
+            int db = 0;
+            int[] indexek = NemNullaTomb(N);
+            int[] ertekek = new int[N];
+
+            int minErtek = MinIndexTomb(ref ADAT, nap, N);
+            for(int i = 0; i < N; i++)
+            {
+                if (ADAT[i,nap] <= minErtek)
+                {
+                    if(!Tartalmaz(i, indexek))
+                    {
+                        indexek[db] = i;
+                        ertekek[db] = ADAT[i, nap];
+                        db++;
+                    }
+                }
+            }
+            int[] kimenetIndex = new int[db];
+            int[] kimenetErteke = new int[db];
+            for(int i = 0; i < db; i++)
+            {
+                kimenetIndex[i] = indexek[i];
+                kimenetErteke[i] = ertekek[i];
+            }
+            BubbleSort(ref kimenetIndex);
+            BubbleSort(ref kimenetErteke);
+            MinTarolo minTarolo = new MinTarolo(kimenetErteke, kimenetIndex);
             return minTarolo;
         }
 
@@ -142,11 +170,15 @@ namespace KomplexFeladat
             for(int i = 0; i < M; i++)
             {
                 MinTarolo minTarolo = MinKeres(i, N,ref ADAT);
-                if(!Tartalmaz(minTarolo.minInd, telepulesek))
+                for(int j = 0; j < minTarolo.minInd.Length; j++)
                 {
-                    telepulesek[db] = minTarolo.minInd;
-                    db++;
+                    if (!Tartalmaz(minTarolo.minInd[j], telepulesek))
+                    {
+                        telepulesek[db] = minTarolo.minInd[j];
+                        db++;
+                    }
                 }
+                
             }
             
             int[] indexKorrigalt = Masol(telepulesek, db);
@@ -165,39 +197,6 @@ namespace KomplexFeladat
                 Console.Write(outputContainer.telepulesIndexek[i] + " ");
             }
         }
-        static void TestKiir(ref int[,]? ADAT,int N, int M)
-        {
-            for(int i = 0; i < N; i++)
-            {
-                for(int j = 0; j < M; j++)
-                {
-                    Console.Write(ADAT[i,j] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static void DebugArray(ref int[] arr)
-        {
-            for (int j = 0; j < arr.Length; j++)
-            {
-                Console.Write(arr[j] + " ");
-            }
-        }
-
-        static void Logic(int N, int M, int[,] data)
-        {
-            for(int i = 0; i<N; i++)
-            {
-                MinTarolo res = MinKeres(i,N,ref data);
-                Console.WriteLine($"{i},{res.minInd},{res.minErt}");
-            }
-        }
-
-        static voidNapiData(int N, int M, int[,] data)
-        {
-
-        }
         static void Main()
         {
             string? inputLine = Console.ReadLine();
@@ -206,7 +205,6 @@ namespace KomplexFeladat
             int M = int.Parse(numbers[1]);
 
             int[,]? ADAT = Beolvas(N,M);
-            //Logic(N,M,ADAT);
             EredmenyTarolo outputContainer = NaponkentiValogatas(N, M, ref ADAT);
             EredmenyKiiras(outputContainer);
 
